d8931a332f2482518a7796fae2c9cca20dc253c8
spencerpomme <spencerpomme@live.com>
2015-04-28 00:40:27 +0800

    multi-function

---
+++ 2_12_7.c
@@ -0,0 +1,20 @@
+/* 2_12_7.c */
+#include <stdio.h>
+void one_three(void);
+void two(void);
+int main(void){
+	printf("starting now:\n");
+	one_three();
+	printf("done!\n");
+	return 0;
+}
+
+void one_three(void){
+	printf("one\n");
+	two();
+	printf("three\n");
+}
+
+void two(void){
+	printf("two\n");
+}
