74fee0b4504d5c3649214436d19ceec1ab72e598
spencerpomme <spencerpomme@live.com>
2015-04-29 16:43:06 +0800

    End of this chapter.

---
+++ 2_12_2.c
@@ -0,0 +1,5 @@
+// 2_12_2.c 
+#include <stdio.h>
+int main(void){
+	printf("Name: Spencer\nAdress: Planet Earth");
+}
---
+++ 2_12_5.c
@@ -0,0 +1,11 @@
+// 2_12_5.c
+#include <stdio.h>
+int main(void){
+	int toes, sum, square;
+	toes = 10;
+	sum = toes + toes;
+	square = toes * toes;
+	printf("toes: %d\nsum: %d\nsquare: %d\n", toes, sum, square);
+	return 0;
+	
+}
