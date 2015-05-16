9db725dedfecc831d66c96164ba807ee20110c3c
spencerpomme <spencerpomme@live.com>
2015-05-16 14:35:27 +0800

    simple one

---
+++ 3.11.3.c
@@ -0,0 +1,7 @@
+/* 3.11.3.c print alarm and sentenses. */
+#include <stdio.h>
+int main(void)
+{
+	printf("\aStartled by the sudden sound, Sally shouted, \"By the Great Pumpkin, what was that!\"\n");
+	return 0;
+}
