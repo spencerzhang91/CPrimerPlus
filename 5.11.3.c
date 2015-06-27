7d2725b88930fbaa7b9a1a12e2242776f9084621
Spencer Zhang <spencerpomme@live.com>
2015-06-27 19:52:45 +0800

    ...

---
+++ 5.11.3.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+#define D2W 7
+
+int main(void)
+{
+	int day, week, wd;
+	printf("Please enter days.\n");
+	scanf("%d", &day);
+	while (day > 0)
+	{
+		week = day / D2W;
+		wd = day % D2W;
+		printf("%d weeks, %d days.\n", week, wd);
+		printf("Continue:\n");
+		scanf("%d", &day);
+	}
+	printf("DONE!\n");
+	return 0;
+}
---
+++ 5.11.4.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+#define C2I 2.54
+#define I2F 12
+
+int main(void)
+{
+	float height, inch, finch;
+	int feet;
+	printf("Enter a height in centimeters (<= 0 to quit): ");
+	scanf("%f", &height);
+	while (height > 0)
+	{
+		inch = height / C2I;
+		feet = (int)inch / I2F;		
+		finch = inch - feet * I2F;
+		printf("%.2f cm = %d feet, %.1f inches.\n", height, feet, finch);
+		printf("Enter a height in centimeters (<= 0 to quit): ");
+		scanf("%f", &height);	
+	}
+	printf("bye\n");
+	return 0;
+}
