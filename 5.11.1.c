6fd36485e3d445db15bbc174931d59d0321b1281
Spencer Zhang <spencerpomme@live.com>
2015-06-27 16:03:09 +0800

    chap 5 excercise

---
+++ 5.11.1.c
@@ -0,0 +1,18 @@
+#define S2M 60
+#include <stdio.h>
+int main(void)
+{
+	int m, h, hm;
+	printf("Please enter time in minutes.\n");
+	scanf("%d", &m);
+	while (m > 0)
+	{
+		h = m / S2M;
+		hm = m % S2M;
+		printf("%d minutes is %d hours, %d minutes.\n", m, h, hm);
+		printf("Continue: \n");
+		scanf("%d", &m);
+	}
+	printf("Done.\n");
+	return 0;
+}
