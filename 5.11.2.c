327352b4f6bd54ace14d33c9a541042db69c23d3
Spencer Zhang <spencerpomme@live.com>
2015-06-27 16:29:24 +0800

    ...

---
+++ 5.11.2.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+int main(void)
+{
+	int num;
+	int n = 0;
+	printf("Please enter a integer.\n");
+	scanf("%d", &num);
+	
+	while (n <= 10)
+	{
+		printf("%d", num + n);
+		printf(" ");
+		n++;
+	}
+	printf("\nEnd of story.\n");
+	return 0;
+}
