541b98f9acde6044bd36c76ff2630c20acb3b68d
Spencer Zhang <spencerpomme@live.com>
2015-07-06 19:56:49 +0800

    ...

---
+++ 7.12.3.c
@@ -0,0 +1,34 @@
+/* 7.12.3.c */
+#include <stdio.h>
+int main(void)
+{
+	int num;
+	int cnt_ev = 0;
+	int cnt_od = 0;
+	int evr_ev, evr_od;
+	int sum_ev = 0;
+	int sum_od = 0;
+	
+	printf("Enter a series of numbers seperated by space. 0 to stop.\n");
+	while ((scanf("%d", &num) == 1) && num != 0)
+	{
+		if (num % 2 == 0)
+		{
+			cnt_ev++;
+			sum_ev += num;
+		}
+		else
+		{
+			cnt_od++;
+			sum_od += num;
+		}
+	}
+	printf("odd number count: %d\n"
+		   "odd number average: %.2f\n"
+		   "even number count: %d\n"
+		   "even number average: %.2f\n",
+		    cnt_od, (float)sum_od / cnt_od,
+			cnt_ev, (float)sum_ev / cnt_ev
+		  );
+	return 0;
+}
