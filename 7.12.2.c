298e2834718c5f5b0baa275100f4f2e5810eb819
Spencer Zhang <spencerpomme@live.com>
2015-07-06 17:03:00 +0800

    chap 7
    just have to use if...else...

---
+++ 7.12.2.c
@@ -0,0 +1,17 @@
+/* 7.12.2.c */
+#include <stdio.h>
+#define COL 9 // actual colum number is COL-1
+int main(void)
+{
+	int count = 0;
+	char ch;
+	while (scanf("%c", &ch) == 1, ch != '#')
+	{
+		if (count++ % COL != 0)
+			printf("%c -> %d  ", ch, ch);
+		else
+			printf("\n");
+	}
+	printf("\nDone.\n");
+	return 0;
+}
