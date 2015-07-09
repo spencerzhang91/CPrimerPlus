bddadaaa69417d2de710a6ba7066a356e76676f4
Spencer Zhang <spencerpomme@live.com>
2015-07-09 12:54:34 +0800

    ...

--- 7.12.8.c
+++ 7.12.8.c
@@ -1,5 +1,6 @@
 /* 7.12.8.c */
 #include <stdio.h>
+#include <stdbool.h>
 #define OTR 1.5
 #define TIME 40
 #define RATE1 0.15
@@ -7,15 +8,16 @@
 #define RATE3 0.25
 #define S1 300
 #define S2 450
+int filter(void);
 
 int main(void)
 {
 	int hours, i;
-	int choice;
 	float netpay = 0;
 	float sumpay = 0;
 	float tax = 0.0;
 	float basepay;
+	
 	
 	for (i = 0; i <= 65; i++)
 		printf("%c", '*');	
@@ -26,46 +28,75 @@ int main(void)
 	for (i; i > 0; i--)
 		printf("%c", '*');
 	printf("\nChoose your base payment per hour:\n");
-	while ((scanf("%d", &choice) == 1) & (choice != 5))
+	
+	switch (filter_pay())
 	{
-		switch (choice)
-		{
-			case 1:
-				basepay = 8.75;
-				break;
-			case 2:
-				basepay = 9.33;
-				break;
-			case 3:
-				basepay = 10.00;
-				break;
-			case 4:
-				basepay = 11.20;
-				break;
-			default:
-				printf("Please enter valid choices(1-5):\n");
-		
-		}
-		printf("\nEnter your working hour a week: ");
-		scanf("%d", &hours);
-		if ((hours <= 40) && (hours >= 0))
-			sumpay = hours * basepay;
-		
-		else if (hours <= 168)
-			sumpay = (hours-TIME)*OTR*basepay + basepay*TIME;
-		
-		if (sumpay <= S1 && sumpay > 0)
-			tax = sumpay * RATE1;
-		
-		else if (sumpay > S1 && sumpay <= S2)
-			tax = S1*RATE1 + (sumpay-S1)*RATE2;
-		
-		else
-			tax = S1*RATE1 + (S2-S1)*RATE2 + (sumpay - S1 - S2)*RATE3;
-		netpay = sumpay - tax;
-		printf("sumpay: %.2f  tax: %.2f  netpay: %.2f\n", sumpay, tax, netpay);
-		printf("\nChoose your base payment per hour again(or 5 to quit):\n");
+		case 1:
+			basepay = 8.75;
+			break;
+		case 2:
+			basepay = 8.75;
+			break;
+		case 3:
+			basepay = 8.75;
+			break;
+		case 4:
+			basepay = 8.75;
+			break;
+		default:
+			printf("You chose to quit, bye!\n");
+			return 0;
 	}
-	printf("You chose to quit, bye!\n");
+	
+	printf("Enter your working hour a week: ");	
+	hours = filter_time();
+	
+	if ((hours <= 40) && (hours >= 0))
+		sumpay = hours * basepay;
+		
+	else if (hours <= 168)
+		sumpay = (hours-TIME)*basepay*OTR + basepay*TIME;
+		
+	if (sumpay <= S1 && sumpay > 0)
+		tax = sumpay * RATE1;
+		
+	else if (sumpay > S1 && sumpay <= S2)
+		tax = S1*RATE1 + (sumpay-S1)*RATE2;
+		
+	else
+		tax = S1*RATE1 + (S2-S1)*RATE2 + (sumpay - S1 - S2)*RATE3;
+	netpay = sumpay - tax;
+	printf("sumpay: %.2f  tax: %.2f  netpay: %.2f\n", sumpay, tax, netpay);
 	return 0;
 }
+
+int filter_pay(void) 
+{
+	int num;
+	char str[40];
+	while ((scanf("%d",&num)!=1) || num > 5 || num < 1)
+	{
+		gets(str);
+		printf("error, input again.\n");
+	}
+ 	while (getchar() != '\n')
+ 		continue;
+	return num;
+}
+
+int filter_time(void) 
+{
+	int num;
+	char str[40];
+	while ((scanf("%d",&num)!=1) || num > 168 || num < 0)
+	{
+		gets(str);
+		printf("error, input again.\n");
+	}
+ 	while (getchar() != '\n')
+ 		continue;
+	return num;
+}
+
+
+
