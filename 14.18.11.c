f455301aca1eb5d1da8666e815deee34d4d7a003
Spencer Zhang <spencerpomme@live.com>
2015-08-16 13:13:59 +0800

    ...

---
+++ 14.18.11.c
@@ -0,0 +1,13 @@
+/* 14.18.11.c */
+#include <stdio.h>
+#include <math.h>
+double transform(double source[], double target[], int n, double (*fp)(double arg));
+int main(void)
+{
+	
+}
+
+double transform(double source[], double target[], int n, double (*fp)(double arg))
+{
+	
+}
