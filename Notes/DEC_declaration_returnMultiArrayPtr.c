9b7200e26b698ee6b08d0f4e65c04ceb5b52232d
Spencer Zhang <spencerpomme@live.com>
2016-01-24 01:57:37 +0800

    JAN24

--- Notes/DEC_declaration_returnMultiArrayPtr.c
+++ Notes/DEC_declaration_returnMultiArrayPtr.c
@@ -7,11 +7,16 @@ int main(void)
 {
     /* This file will test some peculiar delearations and initializations */
     // No.1: char array
-    char array_1[3] = {'a','b','c'};
-    char *array_2 = (char *)malloc(sizeof(char) * 3);
-    for (int i = 0; i < 3; i++)
+    char array_1[3] = {'a','b','c'};                           // ok
+    char *array_2 = (char *)malloc(sizeof(char) * 3);          // ok
+    for (int i = 0; i < 3; i++)                                // initialize
         *(array_2 + i) = 'a' + i;
-    printf("%c", *array_2);
+    
+    // No.2: string array
+    char *array_string[3] = {"abc", "d", "ef"};                // ok
+    printf("%s ", array_string[0]);
+
+
     return 0;
 }
 
