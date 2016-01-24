1e0bbb0fa5d0cfc40da0f0c5e2dce962add93ef6
Spencer Zhang <spencerpomme@live.com>
2016-01-24 14:34:13 +0800

    JAN24

--- Notes/DEC_declaration_returnMultiArrayPtr.c
+++ Notes/DEC_declaration_returnMultiArrayPtr.c
@@ -3,20 +3,33 @@
 #include <stdlib.h>
 #include <math.h>
 
+char **func_rcharPtrArray(void);
+
 int main(void)
 {
     /* This file will test some peculiar delearations and initializations */
+    // ======================= Variable Declearation ==================== //
     // No.1: char array
-    char array_1[3] = {'a','b','c'};                           // ok
-    char *array_2 = (char *)malloc(sizeof(char) * 3);          // ok
-    for (int i = 0; i < 3; i++)                                // initialize
-        *(array_2 + i) = 'a' + i;
+    char array_char_a[3] = {'a','b','c'};                           // ok
+    char *array_char_p = (char *)malloc(sizeof(char) * 3);          // ok
+    for (int i = 0; i < 3; i++)                                     // initialize
+        *(array_char_p + i) = 'a' + i;
     
-    // No.2: string array
-    char *array_string[3] = {"abc", "d", "ef"};                // ok
-    printf("%s ", array_string[0]);
-
-
+    // No.2: array of string pointer
+    char *array_string_a[3] = {"abc", "defg", "hi"};                // ok
+    // No.3: array of string
+    char array_string_2a[3][5] = {"ab","cd","efgh"};                // ok
+    
+    // ====================== Function Calling Area ===================== //
+    char **call_func_no4;
+    call_func_no4 = func_rcharPtrArray();
+    printf("%s", call_func_no4[2]);
     return 0;
 }
 
+// No.4: function that returns array of string pointer
+char **func_rcharPtrArray(void)
+{
+    static char *temp[3] = {"abcd","e","fg"};
+    return temp;
+}
